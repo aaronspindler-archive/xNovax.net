7af21ce537c1e01f92ca305a9490a10b75d4c612
Aaron Spindler <nova1104@live.com>
2019-08-08 23:00:06 -0400

    Removed jobs app

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs.apps.JobsConfig',
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
 ]
