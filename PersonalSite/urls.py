57c9716613bcc4f0be3e2537c8158739b59b9c29
Aaron Spindler <nova1104@live.com>
2019-08-08 22:55:34 -0400

    Moved home page

--- PersonalSite/urls.py
+++ PersonalSite/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+import pages.views
 
 urlpatterns = [
-    path('', jobs.views.home, name='home'),
+    path('', pages.views.home, name='home'),
     path('admin/', admin.site.urls, name='admin'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
