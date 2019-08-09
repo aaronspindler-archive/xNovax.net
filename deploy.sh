b30e22699e5fc5186ef3ca96c7ceda6076605ea7
Aaron Spindler <nova1104@live.com>
2019-08-09 00:11:11 -0400

    Working on deploy script more

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
-echo -n "Deploying PersonalSite"
+echo "Deploying PersonalSite"
 cd PersonalSite
+
+echo "Renaming Files"
 mv settings.py settings_dev.py
 mv settings_prod.py settings.py
 
 cd ..
 git add .
-git commit -m "Working on deploy script"
+echo -n "Enter commit message"
+read commitmsg
+git commit -m "$commitmsg"
 
 git push
 
 cd PersonalSite
+echo "Changing filenames back"
 mv settings.py settings_prod.py
 mv settings_dev.py settings.py
