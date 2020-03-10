0f905f5a74ea975275ba8178aef252c49da59751
Aaron Spindler <aaron@xnovax.net>
2020-03-10 16:08:06 -0400

    Added smaller initial link length

--- utils/random_string.py
+++ utils/random_string.py
@@ -1,6 +1,6 @@
 import random
 
 
-def generate_short(length=5):
+def generate_short(length=1):
 	characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 	return ''.join((random.choice(characters) for _ in range(length)))
