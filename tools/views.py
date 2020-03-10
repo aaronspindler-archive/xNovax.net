954999a2f4750dd6c16a89cfb56c3b3cab14127e
Aaron Spindler <aaron@xnovax.net>
2020-03-10 15:17:45 -0400

    Fixed PK collisions for url shortener

--- tools/views.py
+++ tools/views.py
@@ -11,23 +11,20 @@ def url_shortener(request):
 		form = ShortenURLForm(request.POST)
 		if form.is_valid():
 			shortenedurl = ShortURL()
-
-			# Generate a short string
 			short = random_string.generate_short()
 
-			# Check if the short string already exists
-			check = ShortURL.objects.filter(short=short)
-			# If not save to DB
-			if check.count() == 0:
-				shortenedurl.short = short
-				shortenedurl.target = form.cleaned_data['url']
-				shortenedurl.save()
+			while True:
+				# Check if the short string already exists
+				check = ShortURL.objects.filter(short=short)
+				# If not save to DB
+				if check.count() == 0:
+					shortenedurl.short = short
+					shortenedurl.target = form.cleaned_data['url']
+					shortenedurl.save()
+					return render(request, 'tools/url_shortened.html', {'shortenedurl': shortenedurl})
 
-				return render(request, 'tools/url_shortened.html', {'shortenedurl': shortenedurl})
-
-			# If it does, generate another with length + 1
-			short = random_string.generate_short(len(short) + 1)
-
+				# If it does, generate another with length + 1
+				short = random_string.generate_short(len(short) + 1)
 
 	return render(request, 'tools/url_shortener.html', {'form': form})
 
