1b79c58e00713e0fd5167d267bcb5c637d6e8746
Aaron Spindler <aaron@xnovax.net>
2020-08-29 21:46:04 -0400

    Changed post date display

--- pages/views.py
+++ pages/views.py
@@ -5,9 +5,16 @@ from courses.models import Course
 from projects.models import Project, Technology
 from utils import medium
 
+from datetime import datetime
+
 
 def home(request):
 	posts = medium.get_posts('aaron_spindler', 3)
+
+	for post in posts:
+		pubDate = datetime.strptime(post['pubDate'], '%Y-%m-%d %H:%M:%S')
+		post['pubDate'] = pubDate
+
 	projects = Project.objects.all()[:3]
 	books = Book.objects.all()[:3]
 	courses = Course.objects.all()[:3]
