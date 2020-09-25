5dcbbd523631d8b58d64ae05052bb5e10620e934
Aaron Spindler <aaron@xnovax.net>
2020-09-25 09:25:55 -0400

    Updated gallery admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Event, Photo
 
-admin.site.register(Event)
-admin.site.register(Photo)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'name')
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'event', 'image')
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Photo, PhotoAdmin)
