7a988961a697c89443b1e2f6b3487a6c45c52b42
Aaron Spindler <aaron@xnovax.net>
2020-09-25 09:24:34 -0400

    Updated course admin

--- courses/admin.py
+++ courses/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Course
 
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instituition', 'course_code', 'external_link', 'certificate_link')
+
+
+admin.site.register(Course, CourseAdmin)
