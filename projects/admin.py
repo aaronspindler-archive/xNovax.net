e2392194e73c657072eb8d253820cdd44e93b841
Aaron Spindler <aaron@xnovax.net>
2020-09-25 09:29:50 -0400

    Updated projects admin

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Project, Technology
 
-admin.site.register(Project)
-admin.site.register(Technology)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_description', 'description', 'github_link', 'deployment_link')
+
+
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Technology, TechnologyAdmin)
--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 class Technology(models.Model):
 	name = models.CharField(max_length=500)
 
+	class Meta:
+		verbose_name = "Technology"
+		verbose_name_plural = "Technologies"
+
 	def __str__(self):
 		return self.name
 
