4f86d93895177ddf9d3fecf7cf0bfe0cdce244ec
Aaron Spindler <aaron@xnovax.net>
2020-09-25 09:22:06 -0400

    Updated admin for books

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'author', 'favourite_passage', 'cover')
+
+
+admin.site.register(Book, BookAdmin)
